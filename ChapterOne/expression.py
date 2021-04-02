355b87950cf886ea2590a31881a227a813fdd76a
akenz1901 <michael_aka1@icloud.com>
2021-04-02 23:33:06 +0100

    New Exercises

---
+++ ChapterOne/expression.py
@@ -0,0 +1,5 @@
+addition = 2+4
+difference = 4-3
+multiplication = 8*7
+quotient = 30/2
+print('\n',addition,"\n", difference,'\n', multiplication,'\n', quotient)
---
+++ ChapterOne/printOutPut.py
@@ -0,0 +1,5 @@
+name = "Michael"
+print(name)
+
+newName = str(input("Enter Name: "))
+print(newName)
