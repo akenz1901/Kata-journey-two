17afcd6c5dfce526789b7395add4f2883ed45129
akenz1901 <michael_aka1@icloud.com>
2021-04-12 20:36:45 +0000

    Use contact can now be displayed after several input

--- ChapterOne/user_contact.py
+++ ChapterOne/user_contact.py
@@ -1,11 +1,12 @@
 new_range = 0
 new_details = {}
+list_of_Clients = []
 
 while new_range < 3:
     user_name = input(str("Enter Name: "))
     user_age = input(str("Enter Your Age: "))
     user_phone = input(str("Enter Phone: "))
     details = {'name': user_name, 'age': user_age, 'phone': user_phone}
-    new_details = details
-    new_range +=1
-print(new_details)
+    list_of_Clients.append(details)
+    new_range += 1
+print(list_of_Clients)
