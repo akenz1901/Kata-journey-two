bd63225273b8ddc0a4b2f6d08b058d328d952e83
akenz1901 <michael_aka1@icloud.com>
2021-07-08 23:07:56 +0100

    using string methods

---
+++ chapter_three/string_methods.py
@@ -0,0 +1,13 @@
+from math import *
+
+name = "Evans Michael"
+print(name.index(""))
+print(name.replace("Michael", "Igbo"))
+
+negative_digit = -5
+decimal_numbers = 9.2
+print("absolute number of ", negative_digit, abs(negative_digit))
+print("The round figure of a number", decimal_numbers, round(decimal_numbers))
+print(max(1, 2, 0))
+print(pow(2, 3))
+print("This round a number too", ceil(negative_digit))
