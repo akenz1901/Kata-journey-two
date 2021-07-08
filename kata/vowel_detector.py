79998cc8e8cfd96c26f9646ea36c46408ad0095d
akenz1901 <michael_aka1@icloud.com>
2021-07-08 23:17:24 +0100

    vowels

---
+++ kata/vowel_detector.py
@@ -0,0 +1,12 @@
+def confirm_vowels():
+    message = input("Enter a message: ").lower()
+    vowel = ['a', 'e', 'i', 'o', 'u']
+    is_there_vowel = True
+    empty_container = []
+    for i in message:
+        empty_container.append(i)
+
+    for 
+    return empty_container
+
+
