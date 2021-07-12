7d723f70ecba90de8ee138fca318d1b489f75b11
akenz1901 <michael_aka1@icloud.com>
2021-07-12 14:37:15 +0100

    test

--- kata/tests/duplicate_test.py
+++ kata/tests/duplicate_test.py
@@ -1,8 +1,10 @@
 import unittest
+from kata import duplicateEliminator
+
 
 class MyTestCase(unittest.TestCase):
     def test_something(self):
         self.assertEqual(True, False)
 
-if __name__ == '__main__':
-    unittest.main()
+    def test_duplicate(self):
+        self.assertEqual(duplicateEliminator.removeDuplicate(), [0, 1, 2, 3, 4])
