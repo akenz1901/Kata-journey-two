2d2831699b60c05e9e0213392d96c1cf74f801f4
akenz1901 <michael_aka1@icloud.com>
2021-07-29 18:09:22 +0100

    Revision on strings with practical

--- kata/strings_and_its_functions.py
+++ kata/strings_and_its_functions.py
@@ -25,6 +25,23 @@ def split(sentence):
     return new_sentence
 
 
-def eliminate_punctuation():
-    punctuations = "!@#$%^&*()_-|\><'.;:"
-    
+def eliminate_punctuation(sentence):
+    punctuations = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"
+    new_sentence = ""
+
+    for char in sentence:
+        if char not in punctuations:
+            new_sentence += char
+    return new_sentence
+
+
+sentence = """Pythons are constrictors, which means that they will 'squeeze' the life
+                           out of their prey. They coil themselves around their prey and with
+                           each breath the creature takes the snake will squeeze a little tighter
+                           until they stop breathing completely. Once the heart stops the prey
+                           is swallowed whole. The entire animal is digested in the snake's
+                           stomach except for fur or feathers. What do you think happens to the fur,
+                           feathers, beaks, and eggshells? The 'extra stuff' gets passed out as ---
+                           you guessed it --- snake POOP! """
+result = eliminate_punctuation(sentence).split(sentence)
+print(result)
