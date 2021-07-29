9610a93ee686416faff4a4d6e8474201488558be
akenz1901 <michael_aka1@icloud.com>
2021-07-29 18:21:26 +0100

    Unit test

--- tests/vowel_detector_test.py
+++ tests/vowel_detector_test.py
@@ -7,3 +7,4 @@ class VowelDetectorTest(unittest.TestCase):
     def test_for_vowel_extraction(self):
         alphabeth = "honorable"
         self.assertEqual(vowel_detector.confirm_vowels(alphabeth), ['h', 'n', 'r', 'b', 'l'])
+        self.assertEqual(vowel_detector.remove_vowel(alphabeth), 'hnrbl')
