07c9a27ecc9894f11c3fcf28612263d92077c1ff
akenz1901 <michael_aka1@icloud.com>
2021-09-13 22:58:46 +0100

    Tuple Practice

---
+++ tuples/tuple_declaration.py
@@ -0,0 +1,5 @@
+student = ("akenz", "kelvin", 1995, 12, 3, "Lagos", "Nigeria")
+print(student)
+
+(first_name, last_name, year_of_birth, month, day, state_of_origin, nationality) = student
+print(first_name)
