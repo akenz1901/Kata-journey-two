7700a63d24c96536ddc6e5519a978f472e356899
akenz1901 <michael_aka1@icloud.com>
2021-09-15 21:55:29 +0100

    using tuple and list as a record

---
+++ tuples/tuple_in_list.py
@@ -0,0 +1,5 @@
+students = [("Akenz", ["Cit323", 'Cit321', 'Cit341']),
+            ("Michael", ["Cit381"]),
+            ("Babashola", ["crd305", 'Fms321'])]
+for a, b in students:
+    print("{0} has {1} courses".format(a, len(b)))
