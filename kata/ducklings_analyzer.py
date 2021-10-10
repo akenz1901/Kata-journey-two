45f74a88d5c621869c4a0ce8859b5460f5d376fc
akenz1901 <michael_aka1@icloud.com>
2021-10-10 23:52:31 +0100

    Modified a name

--- kata/ducklings_analyzer.py
+++ kata/ducklings_analyzer.py
@@ -4,7 +4,7 @@ distinguished = 'U'
 
 for letter in prexies:
     if letter == 'O' or letter == 'Q':
-        conc = letter + distinguished
-        print(conc + left_characters, end=".\n")
+        cocant = letter + distinguished
+        print(cocant + left_characters, end=".\n")
     else:
         print(letter + left_characters, end=".\n")
