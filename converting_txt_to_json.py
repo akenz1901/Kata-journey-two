8effeb0ffeb546d69ff9bf939d97b3797b619e76
akenz1901 <michael_aka1@icloud.com>
2022-04-02 07:13:38 +0000

    Algorithm Completion

--- converting_txt_to_json.py
+++ converting_txt_to_json.py
@@ -1,10 +1,37 @@
-first_txt = open('nsc508s t1_22-1.txt', 'r')
+def get_list_of_numbers():
+    list_of_number = [str(i) for i in range(1, 11)]
+
+    questions = {dd: {} for dd in list_of_number}
+
+    return questions
 
 
-list_of_number = [str(i) for i in range(1, 11)]
+def get_file_elements():
+    first_txt = open('nsc508s t1_22-1.txt', 'r')
 
-dic = {list_of_number.pop(0): dd.strip() for dd in first_txt}
+    file_index = [lis.strip() for lis in first_txt]
+    first_txt.close()
 
-print(dic)
+    return file_index
 
-first_txt.close()
+
+def analyze_question_answer():
+    option_list = {'question': 'question', 'option1': 'option1', 'option2': 'option2', 'option3': 'option3',
+                   'option4': 'option4', 'answer': 'answer'}
+    content = get_file_elements()
+    numbers = get_list_of_numbers()
+    count = 0
+    cou = 0
+
+    for i in numbers:
+
+        for item in option_list:
+            option_list[item] = content[count]
+            count += 1
+        numbers[i] = option_list.copy()
+
+    return numbers
+
+
+print(analyze_question_answer())
+# print(get_list_of_numbers())
