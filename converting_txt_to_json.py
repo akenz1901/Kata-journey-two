95e7e66cdc607e1759eb068d57cd10d5c872b2bc
akenz1901 <michael_aka1@icloud.com>
2022-04-03 17:21:00 +0000

    Gopedia Mobile App algorithm for file conversion

--- converting_txt_to_json.py
+++ converting_txt_to_json.py
@@ -1,5 +1,8 @@
-def get_list_of_numbers():
-    list_of_number = [str(i) for i in range(1, 11)]
+import json
+
+
+def get_list_of_numbers(num: int):
+    list_of_number = [str(i) for i in range(1, num+1)]
 
     questions = {dd: {} for dd in list_of_number}
 
@@ -19,9 +22,8 @@ def analyze_question_answer():
     option_list = {'question': 'question', 'option1': 'option1', 'option2': 'option2', 'option3': 'option3',
                    'option4': 'option4', 'answer': 'answer'}
     content = get_file_elements()
-    numbers = get_list_of_numbers()
+    numbers = get_list_of_numbers(11)
     count = 0
-    cou = 0
 
     for i in numbers:
 
@@ -33,5 +35,14 @@ def analyze_question_answer():
     return numbers
 
 
-print(analyze_question_answer())
-# print(get_list_of_numbers())
+def convert_output_to_json(file_name=''):
+    arrange_dict = analyze_question_answer()
+    json_file_name = file_name + '.json'
+
+    file = open(json_file_name, 'w')
+    json.dump(arrange_dict, file, indent=4, sort_keys=False)
+    file.close()
+
+
+# print(get_file_elements())
+convert_output_to_json("NSC508")
